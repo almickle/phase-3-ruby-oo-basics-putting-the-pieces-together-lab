8b3f712a55310309aa6661e5c4c55a96c66d8b51
Michael Dear <michaelcdear@gmail.com>
2022-08-10 10:56:31 -0600

    commit tree 12516f1254be333d718303668185df294cc37f35

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_accessor :title
 
+    def initialize(title="And Then There Were None")
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
